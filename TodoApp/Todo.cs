c47653d6c0eeac41416b476dc0b4b37cfb8383ae
LarsG <lars.gunnar@jobloop.no>
2023-07-25 07:14:09 +0000

    add Todo model

---
+++ TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+// The Model for Todo items
+// In other words which values a Todo element stores
+public class Todo
+{
+  public int Id {get; set;}
+  public string? Name {get; set;}
+  public bool IsComplete {get; set; }
+}
