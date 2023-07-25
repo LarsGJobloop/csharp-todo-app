e49084cb326c38cce286c247f9b63b177f370f80
LarsG <lars.gunnar@jobloop.no>
2023-07-25 14:36:30 +0000

    add main functions into its own file

--- TodoApp/wwwroot/main.js
+++ TodoApp/wwwroot/main.js
@@ -1,6 +1,22 @@
-import { getAllTodoes } from "./api.js"
+import { getAllTodoes, createTodo } from "./api.js"
 import { createTodoList } from "./create.js"
 
+// Create todo list
 const todoList = await getAllTodoes()
 const todoListRoot = document.getElementById("todoes")
 createTodoList(todoListRoot, todoList)
+
+// Submit Formdata
+const todoForm = document.getElementById("todoForm")
+todoForm.addEventListener("submit", async event => {
+  event.preventDefault()
+
+  const formData = new FormData(event.target)
+
+  let data = {}
+  for( const [key, value] of formData.entries()) {
+    data[key] = value
+  }
+  console.log(data.name)
+  createTodo(data.name)
+})
